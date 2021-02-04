b870143688b0839092cc33d76fe0655cc8bf0552
kaoru-drosera <fdsdteolx@gmail.com>
2021-02-04 12:39:17 +0900

    update: 今度こそBASIC認証 俺はきらわr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  # BASIC認証
+  # http_basic_authenticate_with name:ENV["ybon"], password:ENV["growth00"] if Rails.env.production?
+  http_basic_authenticate_with name:"ybon", password:"growth00"
+
   def hello
     render html: 'hello'
   end
